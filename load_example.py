d80f41575c968cc1ef6d3776b9be15ee608531de
chaiery <yhmyhm@mail.ustc.edu.cn>
2015-10-14 15:47:06 +0800

    first commit

---
+++ load_example.py
@@ -0,0 +1,101 @@
+'''
+Load randomly generated example data into the database
+'''
+from f ask import g
+from fhir.models import db, Resource, User, Client, commit_buffers
+from fhir.indexer import index_resource
+from fhir.fhir_parser import parse_resource
+from fhir.fhir_spec import RESOURCES
+import names
+from argparse import ArgumentParser
+import random
+from functools import partial
+import json
+import os
+
+
+BASEDIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fhir')
+
+class MockG(object):
+    def __init__(self):
+        self._nodep_buffers = {}
+
+BUF = MockG()
+
+RELIABILITIES = ['questionable', 'ongoing', 'ok', 'calibrating', 'early']
+INTERPRETATIONS = [
+    {
+        'code': 'L',
+        'display': 'Below low normal',
+        'system': 'http://hl7.org/fhir/vs/observation-interpretation'
+    }, { 
+        'code': 'IND',
+        'display': 'Intermediate',
+        'system': 'http://hl7.org/fhir/vs/observation-interpretation'
+    }, { 
+        'code': 'H',
+        'display': 'Above high normal',
+        'system': 'http://hl7.org/fhir/vs/observation-interpretation'
+    }, { 
+        'code': 'NEG',
+        'display': 'Negative',
+        'system': 'http://hl7.org/fhir/vs/observation-interpretation'
+    }, { 
+        'code': 'POS',
+        'display': 'Positive',
+        'system': 'http://hl7.org/fhir/vs/observation-interpretation'
+    }
+]
+
+
+def save_resource(resource_type, resource_data):
+    '''
+    save a resource to database and index its elements by search params
+    '''
+    valid, search_elements = parse_resource(resource_type, resource_data)
+    assert valid
+    resource = test_resource(resource_type, resource_data) 
+    index_resource(resource, search_elements, g=BUF)
+    return resource
+
+
+def rand_patient():
+    '''
+    generate random resource and index its elements by search params
+    '''
+    gender = 'female' if random.random() < 0.5 else 'male'
+    first_name = names.get_first_name(gender=gender)
+    last_name = names.get_last_name()
+    full_name = '%s %s'% (first_name, last_name)
+    data = {
+        'resourceType': 'Patient',
+        'text': {
+            'status': 'generated',
+            'div': "<div><p>%s</p></div>"% full_name
+        },
+        'name': [{'text': full_name}],
+        'gender': {
+                'text': gender,
+                'coding': [{
+                    'code': 'F' if gender == 'female' else 'M',
+                    'system': 'http://hl7.org/fhir/v3/AdministrativeGender'}]
+        }
+    }
+    print 'Created Patient called %s'% full_name
+    return save_resource('Patient', data)
+
+
+def init_superuser():
+    superuser = User(email='super')
+    db.session.add(superuser)
+    global test_resource
+    test_resource = partial(Resource, owner_id=superuser.email)  
+
+
+if __name__ == '__main__':
+    from server import app
+    with app.app_context():
+        init_superuser()
+        for _ in xrange(8):
+            rand_patient()
+        commit_buffers(BUF) 
