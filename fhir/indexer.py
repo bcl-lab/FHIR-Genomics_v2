6399e1890430da870553beaeabf893c09e0dd21b
chaiery <yhmyhm@mail.ustc.edu.cn>
2016-01-08 22:07:13 -0500

    delete

--- fhir/indexer.py
+++ fhir/indexer.py
@@ -172,9 +172,7 @@ def get_search_args(resource, spec):
 
 def index_resource(resource, search_elements, g=g):
     resource.add_and_commit()
-    params = []
     for search_param in search_elements:
-        #print search_param
         args = get_search_args(resource, search_param['spec'])
         elements = search_param['elements']
         if len(elements) == 0:
